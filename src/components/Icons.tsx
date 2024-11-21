5a6d00f74d37c3cb515bfb3004368a22fc274a9a
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-11-21 11:29:24 -0600

    Add MailCheckIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -168,6 +168,23 @@ export const MessagesIcon = ({ className, style }: IconProps) => (
   </svg>
 );
 
+export const MailCheckIcon = ({ className, style }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    style={style}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8" />
+    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+    <path d="m16 19 2 2 4-4" />
+  </svg>
+);
 export const EditIcon = ({ className, style }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
