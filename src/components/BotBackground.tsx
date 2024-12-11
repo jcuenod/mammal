5bbbf7a237e5912cee2b4875d741b1b164410cf0
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-12-11 09:00:49 -0600

    Add text to background

--- src/components/BotBackground.tsx
+++ src/components/BotBackground.tsx
@@ -1,7 +1,8 @@
 import { MammalIcon } from "./Icons";
 
 export const BotBackground = () => (
-  <div className="absolute left-0 right-0 top-0 bottom-0 overflow-hidden flex items-center justify-center">
-    <MammalIcon className="w-96 h-96 text-slate-200" />
+  <div className="absolute left-0 right-0 top-0 bottom-0 overflow-hidden flex flex-col items-center justify-center text-slate-200">
+    <MammalIcon className="w-96 h-96" />
+    <div className="text-5xl font-bold font-sans">mammal</div>
   </div>
 );
